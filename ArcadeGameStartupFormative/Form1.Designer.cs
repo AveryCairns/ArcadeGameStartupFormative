fadfd96d45881ccc266695376bc2efbc1e14cc39
temp <temp>
2017-09-18 12:51:48 -0400

    Add project files.

---
+++ ArcadeGameStartupFormative/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ArcadeGameStartupFormative
+{
+    partial class ArcadeGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ArcadeGame));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.playButton1 = new System.Windows.Forms.Button();
+            this.countLabel1 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Black;
+            this.titleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.titleLabel.Font = new System.Drawing.Font("OCR A Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(45, 31);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(188, 24);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Arcade Game";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playButton1
+            // 
+            this.playButton1.BackColor = System.Drawing.Color.Black;
+            this.playButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.playButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton1.Font = new System.Drawing.Font("OCR A Std", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton1.ForeColor = System.Drawing.Color.White;
+            this.playButton1.Location = new System.Drawing.Point(103, 143);
+            this.playButton1.Name = "playButton1";
+            this.playButton1.Size = new System.Drawing.Size(88, 58);
+            this.playButton1.TabIndex = 1;
+            this.playButton1.Text = "Play Game";
+            this.playButton1.UseVisualStyleBackColor = false;
+            this.playButton1.Click += new System.EventHandler(this.playButton1_Click);
+            // 
+            // countLabel1
+            // 
+            this.countLabel1.Font = new System.Drawing.Font("OCR A Std", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countLabel1.ForeColor = System.Drawing.Color.White;
+            this.countLabel1.Location = new System.Drawing.Point(12, 72);
+            this.countLabel1.Name = "countLabel1";
+            this.countLabel1.Size = new System.Drawing.Size(260, 68);
+            this.countLabel1.TabIndex = 2;
+            this.countLabel1.Text = "3";
+            this.countLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countLabel1.Visible = false;
+            // 
+            // ArcadeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.countLabel1);
+            this.Controls.Add(this.playButton1);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ArcadeGame";
+            this.Text = "Arcade Game";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button playButton1;
+        private System.Windows.Forms.Label countLabel1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ ArcadeGameStartupFormative/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+/// Created by Avery
+/// Arcade Start Up Formative
+/// Sept. 13 2017
+namespace ArcadeGameStartupFormative
+{
+    public partial class ArcadeGame : Form
+    {
+        public ArcadeGame()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton1_Click(object sender, EventArgs e)
+        {
+            // Play Sound
+            SoundPlayer player = new SoundPlayer(Properties.Resources.atari);
+            player.Play();
+
+            playButton1.Visible = false;
+            titleLabel.Visible = false;
+            countLabel1.Visible = true;
+
+            countLabel1.Text = "3";
+            Refresh();
+            Thread.Sleep(750);
+
+            countLabel1.Text = "2";
+            Refresh();
+            Thread.Sleep(750);
+
+            countLabel1.Text = "1";
+            Refresh();
+            Thread.Sleep(750);
+
+            // Start Sound
+            SoundPlayer player2 = new SoundPlayer(Properties.Resources.deathRay);
+            player2.Play();
+
+            countLabel1.Text = "Start!";
+            ArcadeGame.ActiveForm.BackColor = Color.Lime;
+            Refresh();
+            Thread.Sleep(750);
+
+            countLabel1.Visible = false;
+
+            // Logo Graphics
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+
+            formGraphics.FillEllipse(drawBrush, 50, 100, 175, 125);
+
+            Font drawFont = new Font("Copperplate Gothic Bold", 80, FontStyle.Bold);
+            Font drawFont2 = new Font("Copperplate Gothic Bold", 26, FontStyle.Bold);
+            Font drawFont3 = new Font("Copperplate Gothic Bold", 10, FontStyle.Bold);
+            SolidBrush drawLettersBrush = new SolidBrush(Color.White);
+
+            // Words for Logo
+            formGraphics.DrawString("C", drawFont, drawLettersBrush, 60, 100);
+            formGraphics.DrawString("Rams", drawFont2, drawLettersBrush, 103, 138);
+
+            // Rotating Words
+            formGraphics.TranslateTransform(250, 90);
+            formGraphics.RotateTransform(90);
+            formGraphics.DrawString("Central Rams Arcade", drawFont3, drawLettersBrush, new Rectangle());
+            formGraphics.ResetTransform();
+        }
+    }
+}
